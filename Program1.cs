2d56ddafdcfcaca11605a28e599735492b45c883
mctavish141 <serega.kiev.ua@gmail.com>
2016-01-24 09:41:24 +0200

    Update Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace Program1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string line1 = "Hello, World!A";
+			string line2 = "";
+			foreach (char ch in line1) {
+				if (Char.IsLetter (ch)) {
+					if (Char.IsLower (ch)) {
+						line2 = string.Concat (line2, Char.ToUpper (ch).ToString ());
+					} else {
+						line2 = string.Concat (line2, Char.ToLower (ch).ToString ());
+					}
+				} else {
+					line2 = string.Concat (line2, ch.ToString ());
+				}
+			}
+			Console.WriteLine (line2);
+		}
+	}
+}
